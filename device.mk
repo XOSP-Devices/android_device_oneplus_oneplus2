0fba271035a99070ffb75735b3d1c3205f6bfdd3
Sascha Nesterovic <nesterovic5@gmail.com>
2017-01-23 19:59:54 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -232,12 +232,10 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 # RIL
-PRODUCT_PACKAGES += \
-    librmnetctl \
-    libxml2
-
-PRODUCT_PACKAGES += telephony-ext
-PRODUCT_BOOT_JARS += telephony-ext
+## PRODUCT_PACKAGES += telephony-ext
+## PRODUCT_BOOT_JARS += telephony-ext
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/telephony/telephony-ext.jar:system/framework/telephony-ext.jar
 
 # Sensors
 PRODUCT_PACKAGES += \
